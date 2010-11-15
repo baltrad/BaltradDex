e8962195c5ec1017fe42acff200230735f338a12
szewczenko <szewczenko@szewczenko.imgw.ad>
2010-11-15 13:57:09 +0100

    1) Finally fixed message sorting algorithm (messages are sorted by date in descending order) 2) BaltradFrame and BaltradFrameHandler classes have been moved to a separate library - baltrad-frame-1.0.1.jar. This solution allows for modification of core frame processing mechanism independently from BaltradDex and other components relying on BaltradFrame and BaltradFrameHandler. 3) Hopefully solved radar subscription error preventing peers from exchanging data. 4) Added drop-down time zone list on node properties page. 5) Added new page displaying detailed information about data file.

--- src/java/eu/baltrad/dex/core/controller/FrameDispatcherController.java
+++ src/java/eu/baltrad/dex/core/controller/FrameDispatcherController.java
@@ -530,7 +530,6 @@ public class FrameDispatcherController extends HttpServlet implements Controller
                         // create file entry
                         FileEntry fileEntry = fileCatalog.store(
                                 tempFile.getAbsolutePath() );
-                        
 
                         // Interface with the Beast framework
                         BltDataMessage message = new BltDataMessage();
