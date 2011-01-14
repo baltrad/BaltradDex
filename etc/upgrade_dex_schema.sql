bfa0615466714e18deac14afbcc4ffe036429a62
szewczenko <szewczenko@szewczenko.imgw.ad>
2011-01-14 09:10:18 +0100

    Ticket 277: Upgrade DB script does not retain information and is not successfully executed

--- etc/upgrade_dex_schema.sql
+++ etc/upgrade_dex_schema.sql
@@ -1,5 +1,5 @@
 /***************************************************************************************************
-Copyright (C) 2009-2010 Institute of Meteorology and Water Management, IMGW
+Copyright (C) 2009-2011 Institute of Meteorology and Water Management, IMGW
 
 This file is part of the BaltradDex software.
 
@@ -10,136 +10,99 @@ the Free Software Foundation, either version 3 of the License, or
 
 BaltradDex is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with the BaltradDex software.  If not, see http://www.gnu.org/licenses.
+along with the BaltradDex software. If not, see http://www.gnu.org/licenses.
 ****************************************************************************************************
-Document   : SQL script upgrading existing BaltradDex schema
-Created on : Nov 25, 2010, 8:59 AM
-Author     : szewczenko
+Document : SQL script upgrading existing BaltradDex schema
+Created on : Jan 14, 2011, 9:09 AM
+Author : szewczenko
 ***************************************************************************************************/
 
 
 CREATE OR REPLACE FUNCTION make_plpgsql()
 RETURNS VOID
-	LANGUAGE SQL
+LANGUAGE SQL
 AS $$
-  	CREATE LANGUAGE plpgsql;
+CREATE LANGUAGE plpgsql;
 $$;
 
 SELECT
-  	CASE
-    	WHEN EXISTS (
-      		SELECT 1 from pg_catalog.pg_language where lanname='plpgsql'
-    	) THEN
-      		NULL
-    	ELSE make_plpgsql()
-  	END;
-
-CREATE OR REPLACE FUNCTION upgrade_dex_schema() RETURNS VOID AS $$
-BEGIN
---	BEGIN
---    	ALTER TABLE dex_delivery_register DROP COLUMN file_name;
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_delivery_register.file_name does not exist';
---  	END;
---	BEGIN
---    	ALTER TABLE dex_delivery_register ADD COLUMN uuid VARCHAR(128) NOT NULL;
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_delivery_register.uuid already exists';
---  	END;
---	BEGIN
---    	ALTER TABLE dex_delivery_register ADD COLUMN user_name VARCHAR(32) NOT NULL;
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_delivery_register.user_name already exists';
---  	END;
---	BEGIN
---    	ALTER TABLE dex_delivery_register ADD COLUMN timestamp TIMESTAMP NOT NULL;
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_delivery_register.timestamp already exists';
---  	END;
---	BEGIN
---    	ALTER TABLE dex_delivery_register ADD COLUMN status VARCHAR(16) NOT NULL;
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_delivery_register.status already exists';
---  	END;
---	BEGIN
---    	ALTER TABLE dex_node_configuration ALTER COLUMN time_zone TYPE VARCHAR(128);
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Failed to alter column dex_node_configuration.time_zone already exists';
---  	END;
---	BEGIN
---    	ALTER TABLE dex_messages DROP COLUMN date;
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_messages.date does not exist';
---  	END;
---	BEGIN
---    	ALTER TABLE dex_messages DROP COLUMN time;
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_messages.time does not exist';
---  	END;
---	BEGIN
---    	ALTER TABLE dex_messages ADD COLUMN timestamp TIMESTAMP NOT NULL;
---  	EXCEPTION
---    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_messages.timestamp already exists';
---  	END;
-
-        BEGIN
-    	ALTER TABLE dex_node_connections DROP COLUMN address;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_node_connections.address does not exist';
-  	END;
-	BEGIN
-    	ALTER TABLE dex_node_connections ADD COLUMN short_address VARCHAR(64) NOT NULL;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_node_connections.short_address already exists';
-  	END;
-        BEGIN
-    	ALTER TABLE dex_node_connections ADD COLUMN port VARCHAR(16) NOT NULL;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_node_connections.port already exists';
-  	END;
-
-        BEGIN
-    	ALTER TABLE dex_node_configuration DROP COLUMN address;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_node_configuration.address does not exist';
-  	END;
-	BEGIN
-    	ALTER TABLE dex_node_configuration ADD COLUMN short_address VARCHAR(64) NOT NULL;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_node_configuration.short_address already exists';
-  	END;
-        BEGIN
-    	ALTER TABLE dex_node_configuration ADD COLUMN port VARCHAR(16) NOT NULL;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_node_configuration.port already exists';
-  	END;
-
-        BEGIN
-    	ALTER TABLE dex_users DROP COLUMN node_address;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_users.node_address does not exist';
-  	END;
-	BEGIN
-    	ALTER TABLE dex_users ADD COLUMN short_address VARCHAR(64) NOT NULL;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_users.short_address already exists';
-  	END;
-        BEGIN
-    	ALTER TABLE dex_users ADD COLUMN port VARCHAR(16) NOT NULL;
-  	EXCEPTION
-    	WHEN OTHERS THEN RAISE NOTICE 'Column dex_users.port already exists';
-  	END;
-
-
+CASE
+WHEN EXISTS (
+SELECT 1 from pg_catalog.pg_language where lanname='plpgsql'
+) THEN
+NULL
+ELSE make_plpgsql()
 END;
-$$ LANGUAGE plpgsql
-;
 
-select upgrade_dex_schema();
+CREATE OR REPLACE FUNCTION split_dex_node_connections_address() RETURNS VOID AS $$
+BEGIN
+PERFORM true FROM information_schema.columns
+WHERE table_name = 'dex_node_connections' AND column_name = 'address';
+IF FOUND THEN
+RAISE NOTICE 'splitting column "address" to "short_address" and "port"';
+ALTER TABLE dex_node_connections ADD COLUMN short_address VARCHAR(64);
+ALTER TABLE dex_node_connections ADD COLUMN port VARCHAR(16);
+UPDATE dex_node_connections SET
+short_address = substring(address from E'://([\\w\.]+):'),
+port = substring(address from E'://[\\w\.]+:(\\d+)/');
+ALTER TABLE dex_node_connections ALTER short_address SET NOT NULL;
+ALTER TABLE dex_node_connections ALTER port SET NOT NULL;
+ALTER TABLE dex_node_connections DROP COLUMN address;
+ELSE
+RAISE NOTICE 'column "address" already split to "short_address" and "port"';
+END IF;
+END;
+$$ LANGUAGE plpgsql;
 
-drop function make_plpgsql();
-drop function upgrade_dex_schema();
+CREATE OR REPLACE FUNCTION split_dex_node_configuration_address() RETURNS VOID AS $$
+BEGIN
+PERFORM true FROM information_schema.columns
+WHERE table_name = 'dex_node_configuration' AND column_name = 'address';
+IF FOUND THEN
+RAISE NOTICE 'splitting column "address" to "short_address" and "port"';
+ALTER TABLE dex_node_configuration ADD COLUMN short_address VARCHAR(64);
+ALTER TABLE dex_node_configuration ADD COLUMN port VARCHAR(16);
+UPDATE dex_node_configuration SET
+short_address = substring(address from E'://([\\w\.]+):'),
+port = substring(address from E'://[\\w\.]+:(\\d+)/');
+ALTER TABLE dex_node_configuration ALTER short_address SET NOT NULL;
+ALTER TABLE dex_node_configuration ALTER port SET NOT NULL;
+ALTER TABLE dex_node_configuration DROP COLUMN address;
+ELSE
+RAISE NOTICE 'column "address" already split to "short_address" and "port"';
+END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION split_dex_users_node_address() RETURNS VOID AS $$
+BEGIN
+PERFORM true FROM information_schema.columns
+WHERE table_name = 'dex_users' AND column_name = 'node_address';
+IF FOUND THEN
+RAISE NOTICE 'splitting column "node_address" to "short_address" and "port"';
+ALTER TABLE dex_users ADD COLUMN short_address VARCHAR(64);
+ALTER TABLE dex_users ADD COLUMN port VARCHAR(16);
+UPDATE dex_users SET
+short_address = substring(node_address from E'://([\\w\.]+):'),
+port = substring(node_address from E'://[\\w\.]+:(\\d+)/');
+ALTER TABLE dex_users ALTER short_address SET NOT NULL;
+ALTER TABLE dex_users ALTER port SET NOT NULL;
+ALTER TABLE dex_users DROP COLUMN node_address;
+ELSE
+RAISE NOTICE 'column "node_address" already split to "short_address" and "port"';
+END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT split_dex_node_connections_address();
+SELECT split_dex_node_configuration_address();
+SELECT split_dex_users_node_address();
+
+DROP FUNCTION make_plpgsql();
+DROP FUNCTION split_dex_node_configuration_address();
+DROP FUNCTION split_dex_node_connections_address();
+DROP FUNCTION split_dex_users_node_address();
