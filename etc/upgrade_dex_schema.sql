81df00dae450e899d8a35536d974a826c9f6b877
Anders Henja <anders@pclx006.(none)>
2011-01-25 12:57:06 +0100

    Ticket 303: handle addresses with '-' correctly in db upgrade scripts.

--- etc/upgrade_dex_schema.sql
+++ etc/upgrade_dex_schema.sql
@@ -47,8 +47,8 @@ RAISE NOTICE 'splitting column "address" to "short_address" and "port"';
 ALTER TABLE dex_node_connections ADD COLUMN short_address VARCHAR(64);
 ALTER TABLE dex_node_connections ADD COLUMN port VARCHAR(16);
 UPDATE dex_node_connections SET
-short_address = substring(address from E'://([\\w\.]+):'),
-port = substring(address from E'://[\\w\.]+:(\\d+)/');
+short_address = substring(address from E'://([\\w\.-]+):'),
+port = substring(address from E'://[\\w\.-]+:(\\d+)/');
 ALTER TABLE dex_node_connections ALTER short_address SET NOT NULL;
 ALTER TABLE dex_node_connections ALTER port SET NOT NULL;
 ALTER TABLE dex_node_connections DROP COLUMN address;
@@ -67,8 +67,8 @@ RAISE NOTICE 'splitting column "address" to "short_address" and "port"';
 ALTER TABLE dex_node_configuration ADD COLUMN short_address VARCHAR(64);
 ALTER TABLE dex_node_configuration ADD COLUMN port VARCHAR(16);
 UPDATE dex_node_configuration SET
-short_address = substring(address from E'://([\\w\.]+):'),
-port = substring(address from E'://[\\w\.]+:(\\d+)/');
+short_address = substring(address from E'://([\\w\.-]+):'),
+port = substring(address from E'://[\\w\.-]+:(\\d+)/');
 ALTER TABLE dex_node_configuration ALTER short_address SET NOT NULL;
 ALTER TABLE dex_node_configuration ALTER port SET NOT NULL;
 ALTER TABLE dex_node_configuration DROP COLUMN address;
@@ -87,8 +87,8 @@ RAISE NOTICE 'splitting column "node_address" to "short_address" and "port"';
 ALTER TABLE dex_users ADD COLUMN short_address VARCHAR(64);
 ALTER TABLE dex_users ADD COLUMN port VARCHAR(16);
 UPDATE dex_users SET
-short_address = substring(node_address from E'://([\\w\.]+):'),
-port = substring(node_address from E'://[\\w\.]+:(\\d+)/');
+short_address = substring(node_address from E'://([\\w\.-]+):'),
+port = substring(node_address from E'://[\\w\.-]+:(\\d+)/');
 ALTER TABLE dex_users ALTER short_address SET NOT NULL;
 ALTER TABLE dex_users ALTER port SET NOT NULL;
 ALTER TABLE dex_users DROP COLUMN node_address;
